d1685f0405c36c637a0b49e136a19003134cc2ad
userSTANRAGE <jokarikop@gmail.com>
2025-09-19 23:45:45 +0400

    UPD

---
+++ src/blocks/promo/promo.js
@@ -0,0 +1,26 @@
+import Swiper from "swiper";
+import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
+
+(() => {
+
+    new Swiper('.promo__swiper', {
+        modules: [Navigation, Pagination, Autoplay, EffectFade],
+        slidesPerView: 1, 
+        spaceBetween: 0,
+        effect: 'fade',
+        loop: true,
+        fadeEffect: {
+            crossFade: true,
+
+        },
+        navigation: {
+            nextEl: document.querySelector('.promo .custom-navigation__next'),
+            prevEl: document.querySelector('.promo .custom-navigation__prev'),
+        },
+        pagination: {
+            el: '.promo .swiper-pagination',
+            clickable: true,
+        }
+    });
+
+})();
--- src/js/blocks.js
+++ src/js/blocks.js
@@ -3,3 +3,4 @@ import "../blocks/articles/articles.js";
 import "../blocks/team/team.js";
 import "../blocks/offers/offers.js";
 import "../blocks/map/map.js";
+import "../blocks/promo/promo.js";
